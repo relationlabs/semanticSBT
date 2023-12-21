05fc2dab240d8ad3a45d0fda6a7480d65f343feb
icp123 <97605038+icp123@users.noreply.github.com>
2023-12-21 15:10:31 +0800

    deploy 2048

--- scripts/template/deployActivity.js
+++ scripts/template/deployActivity.js
@@ -6,34 +6,40 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-const name = 'Activity Template';
-const symbol = 'SBT';
-const baseURI = 'https://api.example.com/v1/';
+const name = 'Relation 2048 SBT';
+const symbol = 'R2048SBT';
+let baseURI = 'https://sbt0.io/sbt/base/';
 const schemaURI = 'ar://pEaI9o8moBFof5IkOSq1qNnl8RuP0edn2BFD1q6vdE4';
 const class_ = ["Activity"];
-const predicate_ = [["participate", 3]];
+const predicate_ = [["participant", 3]];
 
-const myActivity = "Example-Activity";
+const myActivity = "Relation2048";
 const whiteList = ["0x0000000000000000000000000000000000000011","0x0000000000000000000000000000000000000022"]
 
 async function main() {
 
     const [owner] = await ethers.getSigners();
 
-    const SemanticSBTLogic = await hre.ethers.getContractFactory("SemanticSBTLogic");
+    const SemanticSBTLogic = await hre.ethers.getContractFactory("SemanticSBTLogicUpgradeable");
     const semanticSBTLogicLibrary = await SemanticSBTLogic.deploy();
     console.log(`SemanticSBTLogic deployed ,contract address: ${semanticSBTLogicLibrary.address}`);
+
+    // polygon: SemanticSBTLogic deployed ,contract address: 0xb6b10404e70Be418e1f0222CFDDf3692fF0d28B8
+    //mumbai: SemanticSBTLogic deployed ,contract address: 0xB9D0144c60Cfc70f17958Dc7b190908aC7b25091
+    //base SemanticSBTLogic deployed ,contract address: 0x34a336b5F55625eC28B07c6e2334560BE9A12d0e
 
     const contractName = "Activity";
     const MyContract = await hre.ethers.getContractFactory(contractName, {
         libraries: {
-            SemanticSBTLogic: semanticSBTLogicLibrary.address,
+            SemanticSBTLogicUpgradeable: semanticSBTLogicLibrary.address,
         }
     });
     const myContract = await MyContract.deploy();
-    console.log(`${contractName} deployed ,contract address: ${myContract.address}`);
+    console.log(`Activity deployed ,contract address: ${myContract.address}`);
     await myContract.deployTransaction.wait();
 
+    baseURI = baseURI+ myContract.address + '/json/';
+    console.log(`baseURI: ${baseURI}`)
     await (await myContract.initialize(
         owner.address,
         name,
@@ -42,22 +48,26 @@ async function main() {
         schemaURI,
         class_,
         predicate_)).wait();
-    console.log(`${contractName} initialized!`);
+    console.log(`Activity initialized!`);
 
 
     await (await myContract.setActivity(myActivity)).wait()
     console.log(`The activity of ${myActivity} is set successfully!`);
 
-    whiteList.push(owner.address)
-    await (await myContract.addWhiteList(whiteList)).wait();
-    console.log(`The whiteList is set successfully!`);
+    // whiteList.push(owner.address)
+    // await (await myContract.addWhiteList(whiteList)).wait();
+    // console.log(`The whiteList is set successfully!`);
 
-    await (await myContract.mint()).wait();
-    console.log(`${owner.address} participate the activity successfully!`);
+    await (await myContract.setFreeMintable(true)).wait();
+    console.log(`setFreeMintable successfully!`);
 
-    const rdf = await myContract.rdfOf(1);
-    console.log(`The rdf of the first token is:  ${rdf}`);
+    // await (await myContract.mint()).wait();
+    // console.log(`${owner.address} participant the activity successfully!`);
 
+    // const rdf = await myContract.rdfOf(1);
+    // console.log(`The rdf of the first token is:  ${rdf}`);
+
+//    npx hardhat verify --network mumbai 0x9B42c6ab3BFA566Da0a0B60CB5C646928eaa3c2b
 }
 
 // We recommend this pattern to be able to use async/await everywhere
